fcb553784c2730281a203e4e28a9316e89551f1b
Tina-1300 <tina.xytrfgthuji1348@gmail.com>
2025-10-25 11:10:48 +0200

    update: build.bat

--- build.bat
+++ build.bat
@@ -1,52 +1,25 @@
 @echo off
-setlocal
 
-:start_loop
+:label
 
 IF EXIST build (
     echo Deleting the build folder...
     rmdir /s /q build
-    PAUSE
+    pause
 )
+
 
 echo Creating the build folder...
 mkdir build
 
-pushd build
-IF %ERRORLEVEL% NEQ 0 (
-    echo Error: Unable to enter build folder.
-    goto :end_script
-)
-
-echo CMake configuration...
-cmake .. -DCMAKE_BUILD_TYPE=Release
-IF %ERRORLEVEL% NEQ 0 (
-    echo CMake configuration error.
-    popd
-    goto :end_script
-)
-
-echo Compilation CMake...
-cmake --build .
-IF %ERRORLEVEL% NEQ 0 (
-    echo CMake compilation error.
-    popd
-    goto :end_script
-)
-
-echo Installation CMake...
+cd build
+cmake -G "MinGW Makefiles" ..
+cmake --build . --config Release
+ctest --output-on-failure
 cmake --install .
-IF %ERRORLEVEL% NEQ 0 (
-    echo CMake installation error.
-    popd
-    goto :end_script
-)
 
-popd
+pause
 
-PAUSE
-goto :start_loop
+cd ../
 
-:end_script
-PAUSE
-endlocal
+goto label
